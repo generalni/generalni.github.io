392ce73dd6d4167c95919319b3ef90c7727eff7f
Nikola Stojanovic <35263124+generalni@users.noreply.github.com>
2018-07-28 14:37:15 +0200

    opeglano

--- projekat-2/js/script.js
+++ projekat-2/js/script.js
@@ -1,36 +1,53 @@
-// if there is no localStorage data, initialize array, else, fill the array with localStorage data
-if (!localStorage.getItem("task")) {
-    arrayTask = [];
-} else {
-    arrayTask = localStorage.getItem("task").split(",");
+window.onload = function() {
+    // if there is no localStorage data, initialize array, else, fill the array with localStorage data
+    if (!localStorage.getItem("task")) {
+        arrayTask = [];
+    } else {
+        arrayTask = localStorage.getItem("task").split(",");
+    }
+
+    // show the tasks from local storage
+    showTasks(arrayTask);
+
+    // add a task if 'enter' is pressed
+    window.onkeydown = function(event) {
+        if (event.keyCode == 13) {
+            addTask();
+        }
+    }
+    document.formNewTask.btnNewTask.addEventListener("click", addTask);
+    document.formTaskList.txtFilterTasks.addEventListener("keyup", filterTasks);
+    document.formTaskList.btnClearTasks.addEventListener("click", clearAllTasks);
 }
 
 // add a single task to localStorage
 function addTask() {
-    let obj = document.taskList.txtNewTask;
+    let newTaskTextField = document.formNewTask.txtNewTask;
 
-    if (obj.value !== "") {
-        arrayTask.push(obj.value);
+    if (newTaskTextField.value !== "") {
+        arrayTask.push(newTaskTextField.value);
         localStorage.setItem("task", arrayTask);
-        showTasks();
-        obj.value = "";
+        showTasks(arrayTask);
+        newTaskTextField.value = "";
+        newTaskTextField.focus();
+        document.formTaskList.txtFilterTasks.value = "";
     } else {
         alert("Task text field cannot be blank");
     }
 }
 
 // show tasks from localStorage
-function showTasks() {
+function showTasks(arrayToShow) {
     document.getElementById("tasksContainer").innerHTML = "";
 
     if (typeof(Storage) !== "undefined") {
-        for (let i = 0; i < arrayTask.length; i++) {
+        for (let i = 0; i < arrayToShow.length; i++) {
             // create single task container
             let taskContainer = document.createElement("DIV");
             taskContainer.className = "singleTask";
             // create single task text content
             let taskSpan = document.createElement("SPAN");
-            let taskContent = document.createTextNode(arrayTask[i]);
+            let taskContent = document.createTextNode(arrayToShow[i]);
             taskSpan.appendChild(taskContent);
             taskContainer.appendChild(taskSpan);
             // create single task delete button
@@ -42,26 +59,17 @@ function showTasks() {
             // add everything to list of tasks
             document.getElementById("tasksContainer").appendChild(taskContainer);
             // clear new task text box
-            document.taskList.txtNewTask.value = "";
+            document.formNewTask.txtNewTask.value = "";
         }
     } else {
         alert("Sorry! Your browser doesn't support Web Storage, all changes will be lost when you turn off your browser.");
     }
 }
 
-// filter tasks
 function filterTasks() {
-    let txtToFilter = document.tasksForm.txtFilterTasks.value.toUpperCase();
-    let groupOfTasks = document.getElementById("tasksContainer").getElementsByTagName("DIV");
-
-    for (let i = 0; i < groupOfTasks.length; i++) {
-        let data = groupOfTasks[i].getElementsByTagName("SPAN")[0];
-        if (data.innerHTML.toUpperCase().indexOf(txtToFilter) > -1) {
-            groupOfTasks[i].style.display = "flex";
-        } else {
-            groupOfTasks[i].style.display = "none";
-        }        
-    }
+    let txtToFilter = document.formTaskList.txtFilterTasks.value.toUpperCase();
+    const filteredArray = arrayTask.filter(word => word.toUpperCase().indexOf(txtToFilter) > -1);
+    showTasks(filteredArray);
 }
 
 // delete all tasks from screen and from localStorage
@@ -75,7 +83,7 @@ function clearAllTasks() {
         // clear task container
         taskContainer.innerHTML = "";
         // clear filter text box
-        document.tasksForm.txtFilterTasks.value = "";
+        document.formTaskList.txtFilterTasks.value = "";
     }
 }
 
@@ -84,16 +92,21 @@ function deleteTask(event) {
     if (confirm("Do you really want to delete this task?")) {
         let objParent = event.target.parentElement;
 
-        // delete selected element
+        // delete selected element from memory
         for (let i = 0; i < arrayTask.length; i++) {
             if (objParent.getElementsByTagName("SPAN")[0].innerHTML === arrayTask[i]) {
                 arrayTask.splice(i, 1);
                 localStorage.setItem("task", arrayTask);
+                break; // necessary if there are several identical task entries
             }
         }
+        // remove selected element from DOM
         objParent.remove();
-        showTasks();
+        // if there is something in filter text box, show only filtered results, else, show the whole list of tasks
+        if (document.formTaskList.txtFilterTasks.value != "") {
+            filterTasks();
+        } else {
+            showTasks(arrayTask);
+        }
     }
 }
-
-window.onload = showTasks;
